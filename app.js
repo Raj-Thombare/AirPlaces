4591e58e40a32dbb9154cffd1467d70295910721
Raj Thombare <therajthombare@gmail.com>
2023-07-05 23:00:12 +0530

    fix: CORS error

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Allow-Control-Origin', '*');
+    res.setHeader('Access-Allow-Control-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Allow-Control-Headers', 'Content-Type, Authorization');
+    next();
+})
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);

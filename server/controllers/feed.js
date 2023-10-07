a18fc40b377055cafca6db2194d8425905ad0822
Raj Thombare <therajthombare@gmail.com>
2023-10-07 23:29:17 +0530

    store post in db

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator");
 
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -27,17 +29,23 @@ exports.createPost = (req, res, next) => {
   }
   const title = req.body.title;
   const content = req.body.content;
-  //create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {
-        name: "Raj Thombare",
-      },
-      createdAt: new Date(),
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/rohit.jpg",
+    creator: {
+      name: "Raj Thombare",
     },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result,
+      });
+    })
+    .catch((err) => [console.log(err)]);
+  //create post in db
 };

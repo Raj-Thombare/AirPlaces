8774f39c3c7c43fcfcc8b7ed0377036e27adb5c8
Raj Thombare <therajthombare@gmail.com>
2023-10-07 15:02:25 +0530

    fix: cors typo

---
+++ server/controllers/feed.js
@@ -0,0 +1,26 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        content: "First Post",
+        Title: "This is the First Post!",
+        imageUrl: "images/rohit.jpg",
+        creator: {
+          name: "Raj Thombare",
+        },
+        createdAt: new Date(),
+      },
+    ],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  //create post in db
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};

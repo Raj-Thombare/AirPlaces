022f5262e31a38c81524e80a255fa976cd6691fe
Raj Thombare <therajthombare@gmail.com>
2023-10-10 21:14:26 +0530

    feat: delete posts from user in db

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -187,6 +187,13 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndRemove(postId);
     })
     .then((result) => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then(() => {
       res.status(200).json({
         message: "Post Deleted!",
       });
